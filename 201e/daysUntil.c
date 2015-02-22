c38f76fe8bf2a25974d1bc13be9aa9c74af7cc5b
LukeStorry <luke.storry@gmail.com>
2015-02-22 13:52:40 +0000

    fixed typos and compiler errors

--- 201e/daysUntil.c
+++ 201e/daysUntil.c
@@ -3,9 +3,13 @@
 //
 //Outputs the number of days until an inputted date, from today's date
 
-
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
 #include <time.h> //I was tempted to do everything without this, just for fun, but coursework deadlines...
+
 
 bool enoughArguments(int num);
 bool valiDate(char **argv);
@@ -13,11 +17,11 @@ bool valiDate(char **argv);
 char *calcTimeDiff(char **argv);
 
 int main ( int argc , char **argv ) {
-    if ! enoughArguments(argc) {
+    if (!enoughArguments(argc)) {
         printf("Not enough arguments, YYYY MM DD required.\n Program will now terminate.\n");
         exit(EXIT_FAILURE);
     };
-    if ! valiDate(argv) {
+    if (!valiDate(argv)) {
         printf("Arguments must be of the form YYYY MM DD.\n Program will now terminate.\n");
         exit(EXIT_FAILURE);
     };
@@ -30,23 +34,23 @@ int main ( int argc , char **argv ) {
 
 
 bool enoughArguments(int num) {
-    if (num === 3) return true;
+    if (num == 3) return true;
     else           return false;   
 }
 
 bool valiDate(char **argv) {
-    valid = true; //innocent until proven guilty, right?
+    bool valid = true; //innocent until proven guilty, right?
     if(strlen(argv[1]) != 4) valid = false;
     for ( int i = 0 ; i < 4 ; i++ )
-       if (! isdigit(argv[1][i]) valid = false;
+       if (!isdigit(argv[1][i])) valid = false;
 
     if(strlen(argv[2]) != 2) valid = false;
     for ( int i = 0 ; i < 2 ; i++ )
-       if (! isdigit(argv[2][i]) valid = false;
+       if (!isdigit(argv[2][i])) valid = false;
     
     if(strlen(argv[3]) != 2) valid = false;
     for ( int i = 0 ; i < 2 ; i++ )
-       if (! isdigit(argv[3][i]) valid = false;
+       if (!isdigit(argv[3][i])) valid = false;
     return valid;
 }
 
