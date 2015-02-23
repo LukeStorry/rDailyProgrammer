8bcfec9095111fc36b499f3fd3374640f59d4236
LukeStorry <luke.storry@gmail.com>
2015-02-23 14:13:12 +0000

    pattern matching for iputtin parameters is working.

--- 200e/FloodFill.hs
+++ 200e/FloodFill.hs
@@ -5,7 +5,7 @@ main = interact $ findString . lines
 findString::[String]->String
 findString strLines
     = let 
-        [w,h]  = map read $ words $ head strLines ::Int
-        [x,y] = map read $ words $ last strLines ::Int
-        c      = read $ last $ last strLines      ::Char
+        [w,h]   = map read $ words $ head strLines ::[Int]
+        [x,y,_] = map read $ words $ last strLines ::[Int]
+        c     = last $ last strLines
       in "" 
