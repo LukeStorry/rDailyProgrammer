605b4f831253f9a86ba14a60453372eeb8ea0a12
LukeStorry <luke.storry@gmail.com>
2015-02-23 15:47:42 +0000

    joined parser into main, looks neater

--- 200e/FloodFill.hs
+++ 200e/FloodFill.hs
@@ -1,16 +1,16 @@
 import Data.List
 
-main = interact $ parser . lines
-
---takes lines from main, parses into args, passes to findString, then outputs result
-parser::[String]->String
-parser input
+main
+    --input <- lines.getContents
     = let 
+        input = lines.getContents
         [w,h]   = map read $ words $ head input  ::[Int]
         [x,y,_] = map read $ words $ last input  ::[Int]
         c       = last $ last input              :: Char
-        array   = tail . init input              ::[Char}
-      in findString w h x y c array
+        array   = tail . init input              ::[[Char]]
+    in interact $ unlines . (fill (w,h) (x,y,c) array) . lines
 
+fill:: (Int,Int)->(Int,Int,Int)->[[Char]]->[[Char]]
+fill (w,h) (x,y,c) array
+    = [['0']]
 
-findString 
