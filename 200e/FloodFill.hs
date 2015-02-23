489a59e3e5b3c96d5863364f5101c59cfd4ea06e
LukeStorry <luke.storry@gmail.com>
2015-02-23 13:49:43 +0000

    added a parser for Ints

--- 200e/FloodFill.hs
+++ 200e/FloodFill.hs
@@ -1,6 +1,13 @@
 
 
-main = interact $ findString lines
+main = interact $ findString . lines
 
 findString::[String]->String
+findString (firstline:rest)
+    = let (w,h) = parseInts firstline
 
+      in "" 
+
+parseInts::String->[Int]
+parseInts "" = []
+parseInts s = map (read . words) s
