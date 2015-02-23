f89c628e68a6047e3a8d2336d0c6d008ac478cb9
LukeStorry <luke.storry@gmail.com>
2015-02-23 17:04:57 +0000

    added recursive replaceChar function

--- 200e/FloodFill.hs
+++ 200e/FloodFill.hs
@@ -19,16 +19,18 @@ main' input
 
 fill::(Int,Int)->(Int,Int,Char,Char)->[[Char]]->[[Char]]
 fill (w,h) (x,y,c,d) array
-    --fill
-    |
-    --check if done
-    | (array !! y) !! x == c    = array
-    | (array !! y) !! x != d    = array
-    --check for edges
+    --fill if still in pool
+    | (array !! y) !! x == d    = replaceChar x y c array
+    | (array !! y) !! x /= d    = array
+    --check for edges, move on
     | x == w-1  = fill (w,h) (x-1,y,c,d) $ fill (w,h) (x,y-1,c,d) $ fill (w,h) (x,y+1,c,d) array
     | x == 0    = fill (w,h) (x+1,y,c,d) $ fill (w,h) (x,y-1,c,d) $ fill (w,h) (x,y+1,c,d) array
     | y == h-1  = fill (w,h) (x-1,y,c,d) $ fill (w,h) (x+1,y,c,d) $ fill (w,h) (x,y-1,c,d) array
     | y == 0    = fill (w,h) (x-1,y,c,d) $ fill (w,h) (x+1,y,c,d) $ fill (w,h) (x,y+1,c,d) array
 
     |otherwise  = fill (w,h) (x-1,y,c,d) $ fill (w,h) (x+1,y,c,d) $ fill (w,h) (x,y-1,c,d) $ fill (w,h) (x,y+1,c,d) array
+
+replaceChar::Int->Int->Char->[[Char]]
+replaceChar 0 0 c [[h:s]:xs] = [[c]++s]++xs
+replaceChar a b c ar =[[" "]]
 
