495accb640c19c0c7d16882a46d9386d6bfe123d
LukeStorry <luke.storry@gmail.com>
2015-02-25 18:19:19 +0000

    changed list of string to one long string

--- 200e/FloodFill.hs
+++ 200e/FloodFill.hs
@@ -8,27 +8,27 @@ main = interact $ unlines . main' . lines
 main'::[String]->[String]        
 main' input 
     = let 
-        [w,h]   = map read $ words $ head input ::[Int]
-        [x,y,_] = map read $ words $ last input ::[Int]
-        array   = tail $ init input             ::[[Char]]
-        c       = last $ last input             ::Char
-        d       = (array !! y) !! x             ::Char
-    in fill (w,h) (x,y,c,d) array
+        [w,h]   = map read $ words $ head input         ::[Int]
+        [x,y,_] = map read $ words $ last input         ::[Int]
+        image   = intercalate "" $ tail $ init input    ::[Char]
+        c       = last $ last input                     ::Char
+        d       = (image !! y) !! x                     ::Char
+    in fill (w,h) (x,y,c,d) image
 
 --maybe add validation checks here later...
 
-fill::(Int,Int)->(Int,Int,Char,Char)->[[Char]]->[[Char]]
-fill (w,h) (x,y,c,d) array
+fill::(Int,Int)->(Int,Int,Char,Char)->[Char]->[Char]
+fill (w,h) (x,y,c,d) image
     --fill if still in pool
-    | (array !! y) !! x == d    = replaceChar x y c array
-    | (array !! y) !! x /= d    = array
+    | (image !! y) !! x == d    = replaceChar x y c image
+    | (image !! y) !! x /= d    = image
     --check for edges, move on
-    | x == w-1  = fill (w,h) (x-1,y,c,d) $ fill (w,h) (x,y-1,c,d) $ fill (w,h) (x,y+1,c,d) array
-    | x == 0    = fill (w,h) (x+1,y,c,d) $ fill (w,h) (x,y-1,c,d) $ fill (w,h) (x,y+1,c,d) array
-    | y == h-1  = fill (w,h) (x-1,y,c,d) $ fill (w,h) (x+1,y,c,d) $ fill (w,h) (x,y-1,c,d) array
-    | y == 0    = fill (w,h) (x-1,y,c,d) $ fill (w,h) (x+1,y,c,d) $ fill (w,h) (x,y+1,c,d) array
+    | x == w-1  = fill (w,h) (x-1,y,c,d) $ fill (w,h) (x,y-1,c,d) $ fill (w,h) (x,y+1,c,d) image
+    | x == 0    = fill (w,h) (x+1,y,c,d) $ fill (w,h) (x,y-1,c,d) $ fill (w,h) (x,y+1,c,d) image
+    | y == h-1  = fill (w,h) (x-1,y,c,d) $ fill (w,h) (x+1,y,c,d) $ fill (w,h) (x,y-1,c,d) image
+    | y == 0    = fill (w,h) (x-1,y,c,d) $ fill (w,h) (x+1,y,c,d) $ fill (w,h) (x,y+1,c,d) image
 
-    |otherwise  = fill (w,h) (x-1,y,c,d) $ fill (w,h) (x+1,y,c,d) $ fill (w,h) (x,y-1,c,d) $ fill (w,h) (x,y+1,c,d) array
+    |otherwise  = fill (w,h) (x-1,y,c,d) $ fill (w,h) (x+1,y,c,d) $ fill (w,h) (x,y-1,c,d) $ fill (w,h) (x,y+1,c,d) image
 
 
 replaceChar::Int->Int->Char->[String]->[String]
