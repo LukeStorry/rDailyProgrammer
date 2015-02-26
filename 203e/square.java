9e7539d6f2363019371e690bdaea74c3c5ff8288
LukeStorry <luke.storry@gmail.com>
2015-02-26 15:49:43 +0000

    started square java

---
+++ 203e/square.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+import java.awt.*;
+class Square {
+        public static void main(
+        setPreferredSize(new Dimension(400, 300));
+    g.fillRect(50, 100, 300, 25);
+  }
+}
