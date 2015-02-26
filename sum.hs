05a90be5c7c9e6d7a3e996f9626b54ed4cf49b63
LukeStorry <luke.storry@gmail.com>
2015-02-26 15:03:27 +0000

    added summing function

--- sum.hs
+++ sum.hs
@@ -1,8 +1,1 @@
-main = interact main'
-
-main'::String->String
-main' input = sum $ map read $ lines input
-
-sum::[Int]->Int
-sum [a] = a
-sum (a:b) = a + sum b
+main = interact $ show . sum . map read . lines
